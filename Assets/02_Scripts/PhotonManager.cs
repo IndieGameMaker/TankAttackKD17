eb94cbdf01e080240f5521e5801914552b6c84ee
LeeJaeHyun <myleje@gmail.com>
2024-11-06 11:31:19 +0900

    Project Init

---
+++ Assets/02_Scripts/PhotonManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
