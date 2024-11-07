fe553b52aeff754e29c700973ac4edf2314987f6
LeeJaeHyun <myleje@gmail.com>
2024-11-07 10:19:18 +0900

    .

--- Assets/02_Scripts/Cannon.cs
+++ Assets/02_Scripts/Cannon.cs
@@ -2,17 +2,19 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-    [SerializeField] private float force = 1500.0f;
-    [SerializeField] private GameObject expEffect;
+    [SerializeField] private CannonDataSO cannonDataSO;
+
+    // [SerializeField] private float force = 1500.0f;
+    // [SerializeField] private GameObject expEffect;
 
     void Start()
     {
-        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * force);
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * cannonDataSO.force);
     }
 
     void OnCollisionEnter()
     {
-        var obj = Instantiate(expEffect, transform.position, Quaternion.identity);
+        var obj = Instantiate(cannonDataSO.expEffect, transform.position, Quaternion.identity);
         Destroy(obj, 5.0f);
 
         Destroy(this.gameObject);
