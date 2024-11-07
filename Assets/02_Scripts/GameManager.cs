1699a1acbb8ae06324acd299054c3c87fec5e3e4
LeeJaeHyun <myleje@gmail.com>
2024-11-07 12:52:52 +0900

    .

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -44,7 +44,7 @@ public class GameManager : MonoBehaviourPunCallbacks
         string connectStr = $"{roomName} (<color=#00ff00>{currentPlayer}</color>/<color=#ff0000>{maxPlayers}</color>)";
         connectionInfoText.text = connectStr;
     }
-
+=
     private void OnExitButtonClick()
     {
         // 룸 Exit 요청
