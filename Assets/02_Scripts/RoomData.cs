056ad318c22cd0ed7da59cfeb7c6ea7fa77d80bd
LeeJaeHyun <myleje@gmail.com>
2024-11-07 15:09:03 +0900

    .

---
+++ Assets/02_Scripts/RoomData.cs
@@ -0,0 +1,25 @@
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+public class RoomData : MonoBehaviour
+{
+    [SerializeField] private TMP_Text roomText;
+
+    private RoomInfo roomInfo;
+
+    // 프로퍼티 선언 
+    // Getter, Setter
+    public RoomInfo RoomInfo
+    {
+        get
+        {
+            return roomInfo;
+        }
+        set
+        {
+            roomInfo = value;
+            roomText.text = $"{roomInfo.Name} : {roomInfo.PlayerCount} / {roomInfo.MaxPlayers}";
+        }
+    }
+}
