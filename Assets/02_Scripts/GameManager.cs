d8621bbdcfa5a06e538764b34af2c03dcfb95711
LeeJaeHyun <myleje@gmail.com>
2024-11-07 12:47:10 +0900

    .

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -34,6 +34,16 @@ public class GameManager : MonoBehaviourPunCallbacks
         PhotonNetwork.Instantiate("Tank", pos, Quaternion.identity);
     }
 
+    private void DisplayConnectInfo()
+    {
+        int currentPlayer = PhotonNetwork.CurrentRoom.PlayerCount;
+        int maxPlayers = PhotonNetwork.CurrentRoom.MaxPlayers;
+        string roomName = PhotonNetwork.CurrentRoom.Name;
+
+        string connectStr = $"{roomName} (<color=#00ff00>{currentPlayer}</color>/<color=#ff0000>{maxPlayers}</color>)";
+        connectionInfoText.text = connectStr;
+    }
+
     private void OnExitButtonClick()
     {
         // 룸 Exit 요청
