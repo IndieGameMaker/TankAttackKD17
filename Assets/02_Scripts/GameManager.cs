7ee33fc766c59fe633d149cf9c67da3a64b3f8c0
LeeJaeHyun <myleje@gmail.com>
2024-11-07 12:36:03 +0900

    .

--- Assets/02_Scripts/GameManager.cs
+++ Assets/02_Scripts/GameManager.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    [Header("UI")]
+    [SerializeField] private Button exitButton;
+
+    [SerializeField] private TMP_Text connectionInfoText;
+
     private void Awake()
     {
     }
 
     IEnumerator Start()
     {
+        exitButton.onClick.AddListener(() => OnExitButtonClick());
+
         yield return new WaitForSeconds(0.5f);
 
         CreateTank();
@@ -23,5 +32,16 @@ public class GameManager : MonoBehaviourPunCallbacks
                                   Random.Range(-100.0f, 100.0f));
 
         PhotonNetwork.Instantiate("Tank", pos, Quaternion.identity);
+    }
+
+    private void OnExitButtonClick()
+    {
+        // 룸 Exit 요청
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Lobby");
     }
 }
